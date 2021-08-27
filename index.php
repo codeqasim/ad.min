7a7a714b065286a0af40a2239fde340a83f1aba5
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-08-27 07:00:12 +0500

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,7 @@
   $page_name = "Dashboard";
   include "partials/_header.php";
   include "partials/_menu.php";
-
- echo phpinfo();
-
- 
-?>
-
+?>                                 
 
 <main role="main" class="container-fluid">
 
